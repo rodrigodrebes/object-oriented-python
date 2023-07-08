b47be4b695b25678adf04d9dd556a16476282dd6
Rodrigo <rodrigodrebes97@gmail.com>
2023-07-08 17:17:09 -0300

    Aula 20 - Composição de classes

---
+++ composição_de_classes/sql_actions/insert.py
@@ -0,0 +1,7 @@
+class Insert:
+    
+    def insert_many(self):
+        print("Insert Many")
+    
+    def insert_one(self):
+        print("Insert One")
---
+++ composição_de_classes/sql_actions/select.py
@@ -0,0 +1,7 @@
+class Select:
+    
+    def select_many(self):
+        print("Select Many")
+    
+    def select_one(self):
+        print("Select One")
